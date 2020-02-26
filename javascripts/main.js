32d540646528786816984bde65feab72590dc273
Sarah Holder <sr.holder@yahoo.com>
2020-02-25 20:41:24 -0600

    created bread function, data file, util

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-Console.log('hi')
+import breads from './components/breads.js';
+
+//div in html - id="bread-container"
+//printToDom function in utils
+//breadData will have array of bread objects, export a getBreads function
+//each bread object = type, color, price, id  
+//breads.js component - with domString builder use getBreads functions
+//import breads components into main
+//event listener on radio button - call breadData.setSelectedBread(breadId)
+//setSelectedBread = assign a variable (selectedBreadId) to the id pass
+const init = () => {
+    breads.makeBreadRadioButtons();
+}
+
+init();
+
